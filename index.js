6229ffa33963d81e2223faf15cc17574042104bf
Juho Vepsalainen <bebraw@gmail.com>
2016-04-02 10:34:59 +0300

    Reformat using two spaces

--- index.js
+++ index.js
@@ -6,47 +6,46 @@ var loaderUtils = require('loader-utils');
 var highlightAuto = hl.highlightAuto;
 var highlight = hl.highlight;
 
-
 module.exports = function(input) {
-    this && this.cacheable && this.cacheable();
-    var query = loaderUtils.parseQuery(this.query);
+  this && this.cacheable && this.cacheable();
+  var query = loaderUtils.parseQuery(this.query);
 
-    if(query.exec) {
-        input = this.exec(input, this.resource);
+  if(query.exec) {
+    input = this.exec(input, this.resource);
+  }
+
+  if(query.raw) {
+    return 'module.exports = ' + JSON.stringify(highlightCode(input, query.lang));
+  }
+
+  var $ = cheerio.load(input);
+
+  $('code').replaceWith(function(i, e) {
+    var $e = $(e);
+    var text = $e.text();
+
+    if(text.split('\n').length < 2) {
+      return $('<code>' + he.encode(text) + '</code>');
     }
 
-    if(query.raw) {
-        return 'module.exports = ' + JSON.stringify(highlightCode(input, query.lang));
-    }
+    var text = $e.text();
+    var klass = $e.attr('class') || '';
+    var lang = klass.split('lang-').filter(id);
+    lang = lang && lang[0];
 
-    var $ = cheerio.load(input);
+    return highlightCode(text, lang);
+  });
 
-    $('code').replaceWith(function(i, e) {
-        var $e = $(e);
-        var text = $e.text();
+  $('pre').addClass('hljs');
 
-        if(text.split('\n').length < 2) {
-          return $('<code>' + he.encode(text) + '</code>');
-        }
-
-        var text = $e.text();
-        var klass = $e.attr('class') || '';
-        var lang = klass.split('lang-').filter(id);
-        lang = lang && lang[0];
-
-        return highlightCode(text, lang);
-    });
-
-    $('pre').addClass('hljs');
-
-    return $.html();
+  return $.html();
 };
 
 function id(a) {return a;}
 
 function highlightCode(code, lang) {
   if(lang) {
-      return highlight(lang, code).value;
+    return highlight(lang, code).value;
   }
 
   return highlightAuto(code).value;
